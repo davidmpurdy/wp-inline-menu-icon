d646de85487f93be9f74d9bcfe63398067ef7211
David Purdy <davidpurdy@gmail.com>
2022-01-01 21:58:38 -0800

    Display icons in menu

--- wp-inline-menu-icon.php
+++ wp-inline-menu-icon.php
@@ -18,3 +18,87 @@
 // save ACF fields for this plugin to the folder inside this plugin
 require_once ('inc/acf-json-location.php');
 new ACF_JSON_Location( plugin_dir_path(__FILE__).'/acf-json/', array("key" => "group_61d12882aa124") );
+
+
+add_filter('wp_nav_menu_objects', 'my_wp_nav_menu_objects', 10, 2);
+/**
+ * Display the icon in the menu title
+ */
+function my_wp_nav_menu_objects( $items, $args ) {
+	
+	// loop
+	foreach( $items as &$item ) {
+		
+		// vars
+		$icon = get_field('icon', $item);
+		
+		
+		// append icon
+		if( $icon && $icon['url'] ) {
+			
+            // set icon height
+            $wrapper_height = '1.5em';
+            $icon_height = get_field('icon_height', $item);
+            $icon_height_units = get_field('icon_height_units', $item);
+            if ($icon_height && $icon_height_units && in_array( $icon_height_units, array('px', 'em', 'rem')) ) {
+                $wrapper_height = $icon_height . $icon_height_units;
+            }
+
+            // set icon position
+            $icon_position_class = '';
+            $icon_position = get_field( 'icon_position', $item );
+            if ($icon_position && in_array( $icon_position, array( 'left', 'replace', 'right')) ) {
+                $icon_position_class = 'icon-' . $icon_position;
+            }
+
+
+            $icon_source = '<span class="inline-menu-icon ' . $icon_position_class . '" style="height:' . $wrapper_height . '">' . file_get_contents($icon['url']) . '</span>';
+
+            if ( $icon_position == 'left' ) {
+			    $item->title = $icon_source . $item->title;
+            }
+            elseif ( $icon_position == 'replace' ) {
+                $item->title = $icon_source;
+            }
+            elseif ( $icon_position = 'right' ) {
+                $item->title = $item->title . $icon_source;
+            }
+			
+		}
+		
+	}
+	
+	
+	// return
+	return $items;
+	
+}
+
+add_action( 'wp_head', 'wp_inline_menu_icon_head_css');
+/**
+ * Add a little CSS to head to control icon position
+ */
+function wp_inline_menu_icon_head_css () {
+    ?>
+
+<style>
+    .inline-menu-icon {
+        display: inline-block;
+    }
+
+    .inline-menu-icon svg {
+        vertical-align: middle;
+        height: 100%;
+        margin-bottom:2pt;
+    }
+
+    .inline-menu-icon.icon-right svg {
+        margin-left: 25%;
+    }
+    .inline-menu-icon.icon-left svg {
+        margin-left: -25%;
+    }
+</script>
+
+    <?php 
+}
